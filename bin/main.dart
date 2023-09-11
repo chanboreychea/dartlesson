e4273bcfe92e952091ff3304604e52e269107960
chanboreychea <cheaborey25@gmail.com>
2023-09-11 10:21:41 +0700

    oop encapsulation

--- bin/main.dart
+++ bin/main.dart
@@ -20,10 +20,29 @@ void main() {
   // Table table = Table();
   // table.display();
 
-  Student student = Student.namedConstructor("John", 20, 1);
-  print("Name: ${student.name}");
-  print("Age: ${student.age}");
-  print("Roll Number: ${student.rollNumber}");
+  // Student student = Student.namedConstructor("John", 20, 1);
+  // print("Name: ${student.name}");
+  // print("Age: ${student.age}");
+  // print("Roll Number: ${student.rollNumber}");
+
+  // Map<String, dynamic> json = {"name": "Dara", "age": 25};
+  // String jsonString = '{"name": "Vong", "age": 30}';
+
+  // Person p1 = Person.fromJson(json);
+  // print("Person 1 name: ${p1.name}");
+  // print("Person 1 age: ${p1.age}");
+  // Person p2 = Person.fromJsonString(jsonString);
+  // print("Person 2 name: ${p2.name}");
+  // print("Person 2 age: ${p2.age}");
+
+  Vehicle v1 = Vehicle();
+  v1.setId(001);
+  v1.model = "Rx7";
+  v1.year = 1998;
+
+  print('id: ${v1.getId()}');
+  print('model: ${v1.model}');
+  print('year: ${v1.year}');
 
   // print('Hello world: ${cal.calculate()}!');
 }
--- lib/oop.dart
+++ lib/oop.dart
@@ -1,3 +1,43 @@
+import 'dart:convert';
+
+class Person {
+  String? name;
+  int? age;
+  Person(this.name, this.age);
+  Person.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    age = json['age'];
+  }
+  Person.fromJsonString(String jsonString) {
+    Map<String, dynamic> json = jsonDecode(jsonString);
+    name = json['name'];
+    age = json['age'];
+  }
+}
+
+//encapsulation get set
+class Vehicle {
+  int? _id;
+  String? _model;
+  int? _year;
+
+  String get model => _model!;
+  set model(String model) => _model = model;
+
+  int get year => _year!;
+  set year(int year) => _year = year;
+
+  // Getter method to access private property _id
+  int getId() {
+    return _id!;
+  }
+
+  // Setter method to update private property _id
+  void setId(int id) {
+    _id = id;
+  }
+}
+
 class Table {
   String? name;
   String? color;
